b91ac290b7163ce86d9fc81d2953b6d68a26aeae
fyc <fycflc@outlook.com>
2022-08-23 18:26:48 +0800

    content_script.js&background.js

--- src/background/main.js
+++ src/background/main.js
@@ -1,1 +1,10 @@
-console.log('hello world background')
+/*global chrome*/
+console.log('This script has been injected')
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+    id: 'log',
+    title: "请选择Tag: %s",
+    contexts: ["selection"]
+    });
+});
