0b3bd572788737e53b76513387928dab640528f0
fyc <fycflc@outlook.com>
2022-08-24 22:57:32 +0800

    get tabs

--- src/background/main.js
+++ src/background/main.js
@@ -1,10 +1,120 @@
 /*global chrome*/
-console.log('This script has been injected')
+async function getCurrentTabs() {
+    // let queryOptions = { active: true, lastFocusedWindow: true };
+    let queryOptions = {};
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let tabsPromise = await chrome.tabs.query(queryOptions);
+    return tabsPromise;
+}
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-    id: 'log',
-    title: "请选择Tag: %s",
-    contexts: ["selection"]
+async function getTabsInformation() {
+
+    const PromiseA = chrome.tabs.onCreated.addListener(() => {
+        setTimeout(() => {
+            getCurrentTabs().then((tabsPromise) => {
+                let tabs = [];
+                for (let tab of tabsPromise){
+                    tabs.push( {
+                            id: tab.id,
+                            title: tab.title,
+                            url: tab.url,
+                        }
+                    )
+                }
+                chrome.storage.local.get({ "list": [] }, function (object) {
+                    let dataList = object["list"];
+                    for (let tab of tabsPromise){
+                        dataList.push(tab);
+                    }
+                    chrome.storage.local.set({ "list": dataList });
+                })
+                console.log(tabs.length);
+                const tabsJson = JSON.stringify(tabs);
+                console.log(tabsJson);
+            });
+        }, 1000)
     });
+    const PromiseB = chrome.tabs.onRemoved.addListener(() => {
+        setTimeout(() => {
+            getCurrentTabs().then((tabsPromise) => {
+                let tabs = [];
+                for (let tab of tabsPromise){
+                    tabs.push( {
+                            id: tab.id,
+                            title: tab.title,
+                            url: tab.url,
+                        }
+                    )
+                }
+                console.log(tabs.length);
+                const tabsJson = JSON.stringify(tabs);
+                console.log(tabsJson);
+            });
+        }, 1000)
+    });
+    const [a, b] = await Promise.all([PromiseA, PromiseB]);
+    return [a, b];
+}
+
+getTabsInformation();
+
+
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId == "log") {
+        chrome.storage.local.get({ "list": [] }, function (object) {
+            let dataList = object["list"];
+            dataList.push(info.selectionText);
+            chrome.storage.local.set({ "list": dataList });
+        })
+    }
 });
+
+// chrome.tabs.onCreated.addListener(() => {
+//     getCurrentTabs().then((tabsPromise) => {
+//         let tabs = {
+//             id: tabsPromise[0].id,
+//             title: tabsPromise[0].title,
+//             url: tabsPromise[0].url,
+//             tag: -1,
+//         };
+//         console.log(tabsPromise.length);
+//         const tabsJson = JSON.stringify(tabs);
+//         console.log(tabsJson);
+//     });
+// });
+// chrome.tabs.onRemoved.addListener(() => {
+//     getCurrentTabs().then((tabsPromise) => {
+//         let tabs = {
+//             id: tabsPromise[0].id,
+//             title: tabsPromise[0].title,
+//             url: tabsPromise[0].url,
+//             tag: -1,
+//         };
+//         console.log(tabsPromise.length);
+//         const tabsJson = JSON.stringify(tabs);
+//         console.log(tabsJson);
+//     });
+// })
+// getCurrentTabs().then((tabsPromise) => {
+//     let tabs = {
+//         id: tabsPromise[0].id,
+//         title: tabsPromise[0].title,
+//         url: tabsPromise[0].url,
+//     };
+//     console.log(tabsPromise.length);
+//     const tabsJson = JSON.stringify(tabs);
+//     console.log(tabsJson);
+// });
+// const tabs = getCurrentTabs();
+// tabs.then()
+// console.log(getCurrentTabs()[0].id);
+
+// let tabs = await chrome.tabs.query()
+// chrome.runtime.onInstalled.addListener(() => {
+//     chrome.contextMenus.create({
+//     id: 'log',
+//     title: "请选择Tag:",
+//     contexts: ["selection"]
+//     });
+// });
--- src/popup/main.js
+++ src/popup/main.js
@@ -3,3 +3,18 @@ import App from './components/App.vue'
 
 const app = createApp(App)
 app.mount('#app')
+
+chrome.storage.local.get({ "list": [] }, function (object) {
+    let dataList = object["list"]
+    if(dataList.length == 0) {
+        let p = document.createElement("p");
+        p.innerText = "暂数据";
+        document.body.appendChild(p);
+        return;
+        }
+    dataList.forEach(function (text) {
+        let div = document.createElement("div");
+        div.innerText = text;
+        document.body.appendChild(div);
+        })
+})
