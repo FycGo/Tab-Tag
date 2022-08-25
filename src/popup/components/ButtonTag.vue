43c3f4186034166f161010dafbb91cd533571ac8
fyc <fycflc@outlook.com>
2022-08-25 12:25:12 +0800

    fixed ButtonTag

--- src/popup/components/ButtonTag.vue
+++ src/popup/components/ButtonTag.vue
@@ -2,19 +2,30 @@
 export default {
   data() {
     return {
+        classType: "buttonOff"
     }
-  }
+  },
+
+  methods: {
+    changeClassType() {
+        if(this.classType == "buttonOff"){
+            this.classType = "buttonOn";
+        }else {
+            this.classType = "buttonOff";
+        }
+    }
+  } 
 }
 </script>
 
 <template>
-  <button class="button">
-    标签页Tag管理
+  <button @click="changeClassType()" :class="classType">
+    Tag标签页管理
   </button>
 </template>
 
 <style scoped>
-.button {
+.buttonOn {
     position: absolute;
     left: 20px;
     top: 20px;
@@ -26,6 +37,21 @@ export default {
     padding: 12px 16px;
     border-radius: 8px;
     background: rgba(2, 86, 255, 0.6);
+    z-index: NaN
+}
+
+.buttonOff {
+    position: absolute;
+    left: 20px;
+    top: 20px;
+    width: 130px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 12px 16px;
+    border-radius: 8px;
+    background: rgb(251, 251, 251);
     z-index: NaN
 }
 </style>
