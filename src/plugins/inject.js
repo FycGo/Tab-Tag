570dd9f216cf31e8e81b57ea24b32c47c8aa2abc
fyc <fycflc@outlook.com>
2022-08-26 18:06:27 +0800

    [inject.js] toggle button

--- src/plugins/inject.js
+++ src/plugins/inject.js
@@ -1,9 +1,9 @@
 console.log("The script has been injected")
-const button = document.createElement('button');
-button.textContent = '请选择Tab:'
-document.body.insertAdjacentElement('afterbegin', button);
+// const button = document.createElement('button');
+// button.textContent = '请选择Tab:'
+// document.body.insertAdjacentElement('afterbegin', button);
 
-// button.addEventListener('click', function () {
+// button.addEventListener('click',https://www.yikm.net/ function () {
 //     chrome.runtime.sendMessage('', {
 //         type: 'notification',
 //     });
